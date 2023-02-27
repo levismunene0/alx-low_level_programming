8746d86525a8d029ffc65950b72e3853928f0a6b
levismunene0 <munenelevis102@gmail.com>
2023-02-27 08:22:42 -0800

    4-print_rev.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,7 @@
  * _strlen - returns the length of a string
  * @s: char to check
  * Description: this will return the length of a string
- * Return  is success
+ * Return: 0  is success
  */
 
 int _strlen(char *s)
