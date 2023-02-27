2c6deb8a66c947adf7249687765e7a4d7c6454d7
levismunene0 <munenelevis102@gmail.com>
2023-02-27 05:54:10 -0800

    0-reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * return_to_98 - takes the pointer and updates the value
- * @n: int to check
- * owned by levismunene0
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: input
+ * Return: n
  */
 
-void reset_to_98 (int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
