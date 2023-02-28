91a2eb84715ce098e8fb5fdcec2ba58fa9463c3a
levismunene0 <munenelevis102@gmail.com>
2023-02-28 00:34:36 -0800

    size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,7 @@ int main(void)
 	long int b;
 	long long int c;
 	char d;
-	float e;
+	float f;
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
