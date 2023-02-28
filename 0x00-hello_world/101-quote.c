55e2d27a1db26fb9e448b5f314b74a157f75ddb8
levismunene0 <munenelevis102@gmail.com>
2023-02-28 00:44:16 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a new line, to standard error
- * Return: always 0
+ * Return: always 1
  */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n"
-		59);
+			"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n"
+			59);
 
 	return (1);
 }
